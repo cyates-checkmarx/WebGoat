cb8d64cec70088ba2c14b8711eb2d1317a31d4f1
Carolyn Yates <carolyn.yates@checkmarx.com>
2024-11-14 13:57:09 -0600

    added test file

---
+++ src/main/java/org/dummy/insecure/framework/test.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
